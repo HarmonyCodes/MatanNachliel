e2ebfcc6e68ee9bbd8d28443baedac0f11628fc1
HarmonyCodes <nechamit887@gmail.com>
2025-05-19 00:23:51 +0300

    first commit

---
+++ server/models/Book.js
@@ -0,0 +1,43 @@
+const mongoose= require('mongoose')
+const Category = {
+    TANACH: `תנ"ך ומפרשיו`,
+    FESTIVALS: "מועדים",
+    THOUGHT: "מחשבה",
+    ETHICS: "מוסר",
+    CHASSIDUT: "חסידות",
+    HALACHA: "הלכה",
+    TALMUD_COMMENTATORS: `מפרשי הש"ס`,
+    BAVLI: "תלמוד בבלי",
+    YERUSHALMI: "תלמוד ירושלמי",
+    MISHNAH: "משניות",
+    SIDDURIM: "סידורים",
+    MISC: "שונות",
+    REFERENCE: "קונקורדנציה, אנציקלופדיות ומילונים"
+  };
+const bookSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    author:{
+        type:String,
+        required:true
+    },
+    subject:{
+        type:String,
+        required:true
+    },
+    category:{
+        type:Category,
+        required:true
+    },
+    notes:{
+        type:String,
+        required:false
+    },
+    image:{
+        type:Image,
+        required:false
+    }
+})
+module.exports = mongoose.model('Book', bookSchema)
